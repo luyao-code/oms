986ff4c52d24721047ce9017a6ca27b584cc94f9
Yao <yao@Yaos-Mac-mini.local>
2023-09-09 00:15:41 -0400

    feat: the first talk between client and server

--- app/app.py
+++ app/app.py
@@ -1,6 +1,21 @@
-from flask import Flask
+from flask import Flask, make_response
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return '<h1>To Do</h1>'
+
+@app.route('/info')
+def get_info():
+    header = {
+        'Access-Control-Allow-Origin': "*"
+    }
+    data = {
+        'name': 'ToDo',
+        'dev': 'Yao'
+    }
+    response = make_response(data)
+    response.headers = header
+    response.status_code = 200
+    return response
--- ui/src/App.js
+++ ui/src/App.js
@@ -1,14 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from "react";
 
 function App() {
+
+  const [message, setMessage] = useState({})
+
+  useEffect(() => {
+    fetch('http://127.0.0.1:5000/info')
+      .then(response => response.json())
+      .then(data => setMessage(data))
+      .catch(error => console.log(error))
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Response from Flask Server: {message.name}, {message.dev} 
         </p>
+        <img src={logo} className="App-logo" alt="logo" />
         <a
           className="App-link"
           href="https://reactjs.org"
