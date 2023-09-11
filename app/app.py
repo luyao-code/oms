c7fdc53db7976568b60ceb4c850b01987e1a0637
Yao <yao@Yaos-Mac-mini.local>
2023-09-11 16:57:20 -0400

    feat: initialized postgreSQL connection

--- app/app.py
+++ app/app.py
@@ -1,6 +1,12 @@
 from flask import Flask, make_response
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+connection_string = "postgresql://postgres:123456@localhost:5432/yao"
+app.config['SQLALCHEMY_DATABASE_URI'] = connection_string
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 @app.route('/')
 def index():
@@ -19,3 +25,10 @@ def get_info():
     response.headers = header
     response.status_code = 200
     return response
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    def __repr__(self):
+        return f'Username: {self.name}'
